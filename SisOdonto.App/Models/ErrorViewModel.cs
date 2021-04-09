f888dfbb9d2af136d89a16f48db3aa4623976747
Luís Chagas <luis.chagas@worthix.com>
2021-04-09 20:39:53 -0300

    Add project files.

---
+++ SisOdonto.App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SisOdonto.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
